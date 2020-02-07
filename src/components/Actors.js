f1ed466b6eb996bcf99eecc2c4351cb566179c32
kevinkim1030 <kevin.kim1030@gmail.com>
2020-02-06 23:20:49 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { actors } from '../data';
 
+let displayedActors = actors.map(actor => {return <div key={actor.name}>
+  <h4>{actor.name}</h4>
+  <ul>{actor.movies.map(movie => <li key={movie}>{movie}</li>)}</ul>
+</div>})
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {displayedActors}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  let displayedDirectors = directors.map(director => {return <div key={director.name}>
+    <h4>{director.name}</h4>
+  <ul>{director.movies.map(movie => <li key={movie}>{movie}</li>)}</ul>
+  </div>})
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {displayedDirectors}
     </div>
   );
 }
